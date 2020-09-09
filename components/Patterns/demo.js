21a1685693ff66ce7322d30b7e1746fcff571a86
Khushboopandit <khushboo17@navgurukul.org>
2020-09-09 18:07:09 +0530

    checking the api

--- components/Patterns/demo.js
+++ components/Patterns/demo.js
@@ -1,27 +1,41 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import {
-  View,
-  StyleSheet,
+    View,
+    StyleSheet,
+    Text
 
 } from 'react-native';
+import { copilot, walkthroughable, CopilotStep } from "react-native-copilot";
 
-export default class Demo extends Component {
-  state = {
-  
-  }
+const WalkthroughableText = walkthroughable(Text);
 
-  render() {
-    return (
-        <View style={styles.container}>
-           
-        </View>
-    );
-  }
+class Demo extends Component {
+    state = {
+
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+               <CopilotStep text="Hey! This is the first step of the tour!" order={1} name="openApp">
+                <WalkthroughableText style={styles.title}>
+                    {'Welcome to the demo of\n"React Native Joyride"'}
+                </WalkthroughableText>
+                </CopilotStep>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
- 
+    container: {
+        flex: 1,
+    },
+    title: {
+        fontSize: 24,
+        textAlign: 'center',
+      },
+
 });
+
+export default copilot()(Demo);
